fc65662bbfd72440b9b35934e8001c6e1ccc160c
Corbo <chmcorbo@gmail.com>
2016-08-06 16:02:43 -0300

    Adicionar as duas classes para implementação do mesmo método, so que agora recursivo.

---
+++ TesteFatorialRecursivo.java
@@ -0,0 +1,7 @@
+public class TesteFatorial {
+		public static void maing(String[] args){
+			Fatorial fat = new Fatorial();
+			int resultado = fat.CalcularFatorial(5);
+			System.out.println(resultado);
+		}
+}
