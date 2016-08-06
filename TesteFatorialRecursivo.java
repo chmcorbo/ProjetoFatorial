ed8a0bfd476e5ca5fe4790711f657171fc3f4c65
Corbo <chmcorbo@gmail.com>
2016-08-06 16:05:07 -0300

    Segunda forma de remoção de arquivos usando a classe TesteFatoriaRecursivo.java

--- TesteFatorialRecursivo.java
+++
@@ -1,7 +0,0 @@
-public class TesteFatorial {
-		public static void maing(String[] args){
-			Fatorial fat = new Fatorial();
-			int resultado = fat.CalcularFatorial(5);
-			System.out.println(resultado);
-		}
-}
